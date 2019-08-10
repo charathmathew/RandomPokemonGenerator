e94dc3eda69d53b12200f3fecea70044fb7f7b16
Mathew Charath <charathmathew@gmail.com>
2019-08-10 11:37:14 -0400

    Initial Commit

---
+++ random-pokemon-generator/README.markdown
@@ -0,0 +1,4 @@
+# Random Pokemon Generator
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/charathm/pen/dBVeGr](https://codepen.io/charathm/pen/dBVeGr).
+
+ 
---
+++ random-pokemon-generator/src/script.js
@@ -0,0 +1,29 @@
+document.getElementById("fetchBtn").onclick = function() {
+  myFunction();
+}
+
+function myFunction() {
+
+  pokeID = Math.floor(Math.random() * 718) + 1;
+  pokeID = pokeID.toString();
+  console.log("Poke ID: " + pokeID);
+
+  url = 'https://pokeapi.co/api/v2/pokemon/' + pokeID;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(json => otherFunction(json));
+}
+
+function otherFunction(json) {
+
+  console.log(json.types[1].type.name);
+  info = "Name: " + json.name + "<br>Type: " + json.types[1].type.name;
+  document.getElementById("info").innerHTML = info;
+
+  imagesrc = json.sprites.front_default;
+  console.log(imagesrc);
+  photo = document.getElementById("photo");
+  photo.src = imagesrc;
+  photo.hidden = false;
+}
